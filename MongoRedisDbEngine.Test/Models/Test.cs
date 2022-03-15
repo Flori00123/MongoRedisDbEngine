b1673a7c16745a9a0d18f750fefc173146d5b793
Florian <sems.bui.group@gmail.com>
2022-03-15 19:31:14 +0100

    Renamed project

--- MongoRedisDbEngine.Test/Models/Test.cs
+++ MongoRedisDbEngine.Test/Models/Test.cs
@@ -1,10 +1,5 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
-using MongoRedisDbEngine.Core;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MongoRedisDbEngine.Models
 {
